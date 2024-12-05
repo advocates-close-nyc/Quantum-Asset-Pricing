d5169c20f71901a1e2d0eafed4961f99f4088454
javaad.ali.ny@gmail.com <javaad101>
2024-12-05 15:57:50 -0600

    Initial Commit

---
+++ Javaad-Ali-Quantum-Asset-Pricing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Javaad-Ali-Quantum-Asset-Pricing
+//
+//  Created by AdvocatesClose on 12/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
